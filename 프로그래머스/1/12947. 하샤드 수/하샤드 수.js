a4d7da7d1257f214089b4afd0bf37ea88f59baec
sumin2710 <154856948+sumin2710@users.noreply.github.com>
2024-01-05 10:46:38 +0900

    [level 1] Title: 하샤드 수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,5 +1,4 @@
 const solution = (x) => {
     const n = (x+"").split("").map((v) => parseInt(v)).reduce((a,c) => a+c);
-    if(x%n === 0) return true;
-    return false;
+    return (x%n==0) ? true : false;
 }

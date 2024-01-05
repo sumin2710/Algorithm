5b99d86c05430ce697d4a5bce8de7750b6487ff2
sumin2710 <154856948+sumin2710@users.noreply.github.com>
2024-01-05 11:06:18 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/README.md
+++ 프로그래머스/1/12943. 콜라츠 추측/README.md
@@ -1,10 +1,10 @@
 # [level 1] 콜라츠 추측 - 12943 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.05 ms
+메모리: 33.6 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 5일 11:0:58
+2024년 1월 5일 11:6:17
 
 ### 문제 설명
 
--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,10 +1,8 @@
-function solution(num) {
-    let cnt = 0;
-    while(cnt !== 500){
-        if(num === 1) return cnt;
-        if(num%2 === 0) num /= 2;
-        else num = num*3 + 1;
+function solution(num){
+    let cnt =0;
+    while(num !== 1 && cnt !== 500){
+        num = (num%2===0) ? num/2 : num*3+1; 
         cnt++;
     }
-    return -1;
+    return (num !== 1) ? -1 : cnt;
 }

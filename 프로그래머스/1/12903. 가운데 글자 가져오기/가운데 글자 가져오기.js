556f4c2aa24aae3cac15fb8c022623801652d68f
sumin2710 <154856948+sumin2710@users.noreply.github.com>
2024-01-08 11:24:52 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,7 +1,9 @@
 const solution = (s) => {
-    let n = s.length;
-    if(n %2 === 0){
-        return [...s].splice(n/2-1, 2).join('');
-    }
-    return [...s].splice(n/2, 1).join('');
+    // let n = s.length;
+    // if(n %2 === 0){
+    //     return [...s].splice(n/2-1, 2).join('');
+    // }
+    // return [...s].splice(n/2, 1).join('');
+    let n = Math.floor(s.length/2);
+    return s.length %2 === 1 ? s[n] : s[n-1]+s[n];
 }

a026301ec2043b3c65843199bd4c77725b9d4e8c
sumin2710 <154856948+sumin2710@users.noreply.github.com>
2024-01-10 09:55:11 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/README.md
+++ 프로그래머스/1/12930. 이상한 문자 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 이상한 문자 만들기 - 12930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.32 ms
+메모리: 33.5 MB, 시간: 0.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 9:48:13
+2024년 1월 3일 9:55:9
 
 ### 문제 설명
 
--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,15 +1,15 @@
 function solution(s) {
-    var answer = '';
-    let list = s.split(' ');
-    let rslt = list.map((word) => {
-        word = word.split('');
-        for(let i=0; i<word.length; i++){
-            if(i % 2===0 || i === 0){
-                word[i] = word[i].toUpperCase();
-            }else{
-                word[i] = word[i].toLowerCase();
-            }
-        }return word.join('');
-    }); 
-    return rslt.join(' ');
+    return s.split(' ').map(word => word.split('').map((w, i) => i % 2 === 0 ? w.toUpperCase() : w.toLowerCase()).join('')).join(' ');
+    // let list = s.split(' ');
+    // let rslt = list.map((word) => {
+    //     word = word.split('');
+    //     for(let i=0; i<word.length; i++){
+    //         if(i % 2===0 || i === 0){
+    //             word[i] = word[i].toUpperCase();
+    //         }else{
+    //             word[i] = word[i].toLowerCase();
+    //         }
+    //     }return word.join('');
+    // }); 
+    // return rslt.join(' ');
 }

931d5c65385db2afa7c25a3aa6008a276dd7b5d5
sumin2710 <154856948+sumin2710@users.noreply.github.com>
2024-01-12 09:34:30 +0900

    [level Lv.1] Title: 시저 암호, Time: 2.00 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,15 @@
+function solution(s, n) {
+    const answer = s.split('').map((ch)=>{
+        const code = ch.charCodeAt();
+        if(97 <= code && code <= 122){
+            return String.fromCharCode((code+n - 97) % 26 + 97)
+        }
+
+        if(65 <= code && code <= 90){
+            return String.fromCharCode((code+n - 65) % 26 + 65)
+        }
+        return ' ';
+    }).join('')
+
+    return answer
+}

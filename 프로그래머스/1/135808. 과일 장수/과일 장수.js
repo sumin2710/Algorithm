d2f165655e26e06919dd7927054660af20fe029c
sumin2710 <154856948+sumin2710@users.noreply.github.com>
2024-01-18 09:40:51 +0900

    [level 1] Title: 과일 장수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,15 @@
+function solution(k, m, score) {
+    if(score.length < m){
+        return 0;
+    }
+    
+    score.sort((a, b) => a - b);
+    
+    let total = 0;  
+    while(score.length >= m){
+        const box = score.splice(score.length - m, m);
+        total += m * box[0];
+    }
+    
+    return total;
+}

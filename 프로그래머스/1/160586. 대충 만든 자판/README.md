ba6bfa21b7a40ba9841dcc089e4d2ac8aef1ddb7
sumin2710 <154856948+sumin2710@users.noreply.github.com>
2024-01-26 12:19:41 +0900

    [level 1] Title: 대충 만든 자판, Time: 8.46 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/README.md
+++ 프로그래머스/1/160586. 대충 만든 자판/README.md
@@ -1,10 +1,10 @@
 # [level 1] 대충 만든 자판 - 160586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160586#) 
 
 ### 성능 요약
 
-메모리: 36.9 MB, 시간: 83.28 ms
+메모리: 36.9 MB, 시간: 8.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 5일 9:57:24
+2024년 1월 5일 12:19:38
 
 ### 문제 설명
 
--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,24 +1,24 @@
 function solution(keymap, targets) {
-    const clickNum = (ch) => {
-        let min = 100;
-        keymap.forEach((key) => {
-            const tmp = [...key].indexOf(ch);
-            if(tmp !== -1 && tmp < min){
-                min = tmp;
+    var answer = new Array(targets.length).fill(-1);
+    // targets 순회
+    for(let i=0; i< targets.length; i++){
+        const target = targets[i];
+        let cnt = 0;
+        // target의 각 문자를 순회 // 'ABCD'
+        for(let j=0; j<target.length; j++){
+            const ch = target[j]; //'A'
+            // keymap의 각 요소 중 ch를 포함하는 문자의 minimum index 구하기
+            const min = Math.min(...keymap.map(v => {
+                const idx = v.indexOf(ch);
+                return idx === -1 ? Infinity : idx+1;
+            })); // idx가 -1(문자열 만들 수 없다)면 Infinity 아니면 idx+1번 클릭 가능
+            if(min === Infinity){ // 문자열 못만들면 
+                cnt = -1; 
+                break;  // target의 각 요소 순회 중지
             }
-        });
-        return min+1;
-    };
-    var answer = Array(targets.length).fill(0);
-    targets.forEach((target, idx) => {
-        for(let ch of [...target]){
-            let tmp = clickNum(ch);
-            console.log(tmp)
-            if(tmp === 101){
-                answer[idx] = -1;
-                break;
-            }else answer[idx] += tmp;
+            cnt += min; // 횟수에 최소 횟수 더해주기
         }
-    })
+        answer[i] = cnt;
+    }
     return answer;
 }

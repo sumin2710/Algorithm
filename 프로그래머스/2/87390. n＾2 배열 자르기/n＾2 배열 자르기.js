2b9886b9f5208abdd1bec337d671157753f6d726
sumin2710 <154856948+sumin2710@users.noreply.github.com>
2024-03-06 11:12:26 +0900

    [level 2] Title: n^2 배열 자르기, Time: 6.58 ms, Memory: 43.9 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -1,13 +1,22 @@
+// function solution(n, left, right) {
+//     var answer = [];
+    
+//     const first_row = Math.floor(left/n+1)
+//     const last_row = Math.floor(right/n+1);
+    
+//     let arr = new Array(n).fill(0).map((v, i) => i+1); // 1234
+//     for(let i=first_row; i<=last_row; i++){ // 2 ~ 4
+//        answer = [...answer, ...arr.fill(i, 0, i)]; // 기존 answer에 추가 -> 0부터 i번째 인덱스까지 i로 채운 arr
+//     }
+ 
+//     return answer.splice(left%n, right-left+1);
+// }
 function solution(n, left, right) {
     var answer = [];
-    
-    const first_row = Math.floor(left/n+1)
-    const last_row = Math.floor(right/n+1);
-    
-    let arr = new Array(n).fill(0).map((v, i) => i+1); // 1234
-    for(let i=first_row; i<=last_row; i++){ // 2 ~ 4
-       answer = [...answer, ...arr.fill(i, 0, i)]; // 기존 answer에 추가 -> 0부터 i번째 인덱스까지 i로 채운 arr
+
+    for (let i = left; i <= right; i++) {
+        answer.push(Math.max(i % n, parseInt(i / n)) + 1)
     }
- 
-    return answer.splice(left%n, right-left+1);
+
+    return answer;
 }

d975fe96cc0ca39d9e258c0c959d1fd765640645
sumin <154856948+sumin2710@users.noreply.github.com>
2024-08-15 06:39:59 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 932 ms, Memory: 105940 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,11 +1,14 @@
+import sys
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(N)]
-point = [list(map(int, input().split())) for _ in range(M)]
 
 prefix = [[0] * (N+1) for _ in range(N+1)]
-for y in range(N):
-    for x in range(N):
-        prefix[y+1][x+1] = prefix[y][x+1] + prefix[y+1][x] - prefix[y][x] + graph[y][x]
+for x in range(N):
+    for y in range(N):
+        prefix[x+1][y+1] = prefix[x][y+1] + prefix[x+1][y] - prefix[x][y] + graph[x][y]
         
-for [y1,x1,y2,x2] in point:
-    print(prefix[y2][x2] - prefix[y2][x1-1] - prefix[y1-1][x2] + prefix[y1-1][x1-1])
+for _ in range(M):
+    x1, y1, x2, y2 = map(int, input().split())
+    print(prefix[x2][y2] - prefix[x2][y1-1] - prefix[x1-1][y2] + prefix[x1-1][y1-1])

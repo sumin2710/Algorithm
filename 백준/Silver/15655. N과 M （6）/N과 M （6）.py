fc1baafe2f91a6336a132bd47f0e90c0b8d023d5
sumin <154856948+sumin2710@users.noreply.github.com>
2024-08-24 17:04:04 +0900

    [Silver III] Title: N과 M (6), Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+li = list(map(int, input().split()))
+li.sort()
+
+arr = []
+def recur(number, start):
+    if number == m:
+      print(*arr)
+      return
+    
+    for i in range(start, n):
+      arr.append(li[i])
+      recur(number+1, i+1)
+      arr.pop()
+
+recur(0, 0)

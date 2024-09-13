9efa3f6fc0752098695488b773cb87f27a142d39
sumin <154856948+sumin2710@users.noreply.github.com>
2024-09-13 19:08:47 +0900

    [Silver II] Title: 스택 수열, Time: 92 ms, Memory: 37344 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -5,31 +5,18 @@ li = [int(input()) for _ in range(n)]
 stack = []
 ans = []
 
-isPossible = True
 idx = 0
 curr = 1 # 현재 push 가능한 수 
-while idx < n:
-  target = li[idx] 
-  for i in range(curr, target+1):
-    stack.append(i)
-    ans.append('+')
-    curr += 1
-
-  if stack:
-    if stack[-1] == target:
-      stack.pop()
-      ans.append('-') 
+for target in li:
+    while curr <= target:
+        stack.append(curr)
+        ans.append('+')
+        curr += 1
+    if stack and stack[-1] == target:
+        stack.pop()
+        ans.append('-')
     else:
-      isPossible = False
-      break
-
-  if isPossible == False:
-    break
-
-  idx += 1
-
-if isPossible:
-  for e in ans:
-    print(e)
-else:
-  print('NO')
+        ans.clear()
+        ans.append('NO')
+        break
+print('\n'.join(ans))
